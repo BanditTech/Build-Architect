e3a2de916967da7acdc6479f3d4becdbb042cbed
BanditTech <thebbandit@gmail.com>
2024-06-12 22:31:42 -0700

    Initial Files

---
+++ Build Architect/Core.lua
@@ -0,0 +1,6 @@
+﻿local AddOnName = ...
+local BA = LibStub("AceAddon-3.0"):GetAddon(AddOnName)
+
+function BA:OnInitialize()
+	BA:DB_INIT()
+end
---
+++ Build Architect/Modules/DB.lua
@@ -0,0 +1,20 @@
+﻿local AddOnName = ...
+local BA = LibStub("AceAddon-3.0"):GetAddon(AddOnName)
+
+local defaults = {
+	global = {
+		builds = {},
+	}
+}
+
+BA.Options = {
+	type = "group",
+	name = AddOnName,
+	args = {},
+};
+
+
+function BA:DB_INIT()
+	self.db = LibStub("AceDB-3.0"):New("Build_Architect_Options", defaults, true)
+	BA.Options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+end
